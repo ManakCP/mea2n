f79df47905a11f7622f217b5e9350df001ac54ee
ManakCP <manakchand@outlook.com>
2017-06-16 09:35:30 +0530

    dashboard initiated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,15 @@ import { AuthService } from './auth.service';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     SignupComponent,
-    routingComponent
+    routingComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
